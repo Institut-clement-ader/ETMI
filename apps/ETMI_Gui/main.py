85d6d83e6087b0803025dfe770719493d953d2a9
[Bird] <tmangeard@gmx.fr>
2023-06-23 13:16:33 +0200

    ui mainwindows & dialog quit

--- apps/ETMI_Gui/main.py
+++ apps/ETMI_Gui/main.py
@@ -1,21 +1,40 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog, QPushButton
 from PySide6.QtCore import QFile, Slot
-from ui_etmi_ui import Ui_MainWindow 
+from ui_etmi_ui import Ui_MainWindow
+from quit_ui import Ui_Dialog_Quit
+
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.Lancer.clicked.connect(self.lancer)
-        
+        self.ui.btnRun.clicked.connect(self.lancer)
+        # self.ui.btnQuit.clicked.connect(self.quit)
+        # self.ui.actionQuit.triggered.connect(self.quit)
 
     @Slot()
     def lancer(self):
         # self.ui.ShowText.appendPlainText(self.ui.lineEdit.text())
         # self.ui.ShowText.appendPlainText('coool')
         print('')
+
+    def closeEvent(self, eventQCloseEvent):
+        dlb = QuitDlg()
+        reply = dlb.exec()
+        if reply == QDialog.DialogCode.Accepted:
+            eventQCloseEvent.accept()
+        else:
+            eventQCloseEvent.ignore()
+
+
+class QuitDlg(QDialog):
+    def __init__(self, parent=None):
+        super(QuitDlg, self).__init__(parent)
+        self.ui = Ui_Dialog_Quit()
+        self.ui.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ apps/ETMI_Gui/quit_ui.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'quit.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QLabel, QSizePolicy, QWidget)
+
+class Ui_Dialog_Quit(object):
+    def setupUi(self, Dialog_Quit):
+        if not Dialog_Quit.objectName():
+            Dialog_Quit.setObjectName(u"Dialog_Quit")
+        Dialog_Quit.setWindowModality(Qt.NonModal)
+        Dialog_Quit.resize(177, 96)
+        Dialog_Quit.setLocale(QLocale(QLocale.French, QLocale.France))
+        self.btn_quit = QDialogButtonBox(Dialog_Quit)
+        self.btn_quit.setObjectName(u"btn_quit")
+        self.btn_quit.setGeometry(QRect(-20, 50, 221, 32))
+        self.btn_quit.setCursor(QCursor(Qt.WaitCursor))
+        self.btn_quit.setToolTipDuration(-3)
+        self.btn_quit.setLocale(QLocale(QLocale.French, QLocale.France))
+        self.btn_quit.setOrientation(Qt.Horizontal)
+        self.btn_quit.setStandardButtons(QDialogButtonBox.No|QDialogButtonBox.Yes)
+        self.btn_quit.setCenterButtons(True)
+        self.lbl_quit = QLabel(Dialog_Quit)
+        self.lbl_quit.setObjectName(u"lbl_quit")
+        self.lbl_quit.setGeometry(QRect(30, 20, 131, 16))
+
+        self.retranslateUi(Dialog_Quit)
+        self.btn_quit.accepted.connect(Dialog_Quit.accept)
+        self.btn_quit.rejected.connect(Dialog_Quit.reject)
+
+        QMetaObject.connectSlotsByName(Dialog_Quit)
+    # setupUi
+
+    def retranslateUi(self, Dialog_Quit):
+        Dialog_Quit.setWindowTitle(QCoreApplication.translate("Dialog_Quit", u"Fermeture", None))
+        self.lbl_quit.setText(QCoreApplication.translate("Dialog_Quit", u"Voulez-vous quittez?", None))
+    # retranslateUi
+
